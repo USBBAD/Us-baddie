3c24e6ad40e2af0d73fd205a14c0fad070287d39
DmitryMurashov <dmtr.murashov@gmail.com>
2023-10-22 15:18:59 +0300

    [stm32f103c6:target] Impl | .bss and .data bringup

--- src/target/stm32f103c6/src/target.c
+++ src/target/stm32f103c6/src/target.c
@@ -7,12 +7,46 @@
 
 #include "target/target.h"
 
-struct Target {
+struct Target
+{
 };
 
 static struct Target sTarget;
 
+static void initializeBss();
+static void initializeData();
+
+/// @brief Sets the entire .bss section w/ NULL
+static void initializeBss()
+{
+	extern long int *gBssStart;
+	extern long int *gBssEnd;
+
+	for (register long int *bssIt = gBssStart; bssIt < gBssEnd; ++bssIt)
+	{
+		*bssIt = 0;
+	}
+}
+
+/// @brief Copies default values for .data section from flash memory
+static void initializeData()
+{
+	extern int *gRamDataStart;
+	extern int *gRamDataEnd;
+	extern int *gFlashDataStart;
+
+	for (register int *flashDataIt = gFlashDataStart, *ramDataIt = gRamDataStart;
+		ramDataIt < gRamDataEnd;
+		++flashDataIt, ++ramDataIt
+	) {
+		*ramDataIt = *flashDataIt;
+	}
+}
+
 TargetHandle targetInitialize()
 {
+	initializeBss();
+	initializeData();
+
 	return (TargetHandle)&sTarget;
 }

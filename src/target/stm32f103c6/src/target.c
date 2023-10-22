5de22072eac3e501449fae1c06f4c47b996acf45
DmitryMurashov <dmtr.murashov@gmail.com>
2023-10-22 15:53:27 +0300

    [stm32f103c6:target] Bug | .bss and .data initialization: treating marker symbols from the linker script as pointers. The address was the required thing

--- src/target/stm32f103c6/src/target.c
+++ src/target/stm32f103c6/src/target.c
@@ -19,10 +19,11 @@ static void initializeData();
 /// @brief Sets the entire .bss section w/ NULL
 static void initializeBss()
 {
-	extern long int *gBssStart;
-	extern long int *gBssEnd;
+	// See the linker script where those symbols are defined
+	extern unsigned long int gBssStart;
+	extern unsigned long int gBssEnd;
 
-	for (register long int *bssIt = gBssStart; bssIt < gBssEnd; ++bssIt)
+	for (volatile register unsigned long int *bssIt = &gBssStart; bssIt < &gBssEnd; ++bssIt)
 	{
 		*bssIt = 0;
 	}
@@ -31,12 +32,13 @@ static void initializeBss()
 /// @brief Copies default values for .data section from flash memory
 static void initializeData()
 {
-	extern int *gRamDataStart;
-	extern int *gRamDataEnd;
-	extern int *gFlashDataStart;
+	// See the linker script where those symbols are defined
+	extern int gRamDataStart;
+	extern int gRamDataEnd;
+	extern int gFlashDataStart;
 
-	for (register int *flashDataIt = gFlashDataStart, *ramDataIt = gRamDataStart;
-		ramDataIt < gRamDataEnd;
+	for (volatile register int *flashDataIt = &gFlashDataStart, *ramDataIt = &gRamDataStart;
+		ramDataIt < &gRamDataEnd;
 		++flashDataIt, ++ramDataIt
 	) {
 		*ramDataIt = *flashDataIt;

6df3a647bac005d454aee15c1cb04a7d5f266c48
DmitryMurashov <dmtr.murashov@gmail.com>
2023-11-12 15:39:45 +0200

    [stm32f103c6:uart] Impl | Moved USART clock enabling into "usart" module

--- src/target/stm32f103c6/src/uart.c
+++ src/target/stm32f103c6/src/uart.c
@@ -5,6 +5,14 @@
 //     Author: Dmitry Murashov (dmtr <DOT> murashov <AT> <GMAIL> <DOT> <COM>)
 //
 
+#include <stm32f103x6.h>
+
+static void configureClock();
+
+static void configureClock()
+{
+}
+
 void uartConfigure()
 {
 	// TODO

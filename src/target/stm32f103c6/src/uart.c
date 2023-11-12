7f6fe78b163df79976f197b26b288f4b5c80f2d0
DmitryMurashov <dmtr.murashov@gmail.com>
2023-11-12 15:03:44 +0200

    [stm32f103c6:uart] Impl | Stub, implementation is WIP

---
+++ src/target/stm32f103c6/src/uart.c
@@ -0,0 +1,11 @@
+//
+// uart.c
+//
+// Created on: Oct 28, 2023
+//     Author: Dmitry Murashov (dmtr <DOT> murashov <AT> <GMAIL> <DOT> <COM>)
+//
+
+void uartConfigure()
+{
+	// TODO
+}
---
+++ src/target/stm32f103c6/src/uart.h
@@ -0,0 +1,13 @@
+//
+// uart.h
+//
+// Created on: Oct 28, 2023
+//     Author: Dmitry Murashov (dmtr <DOT> murashov <AT> <GMAIL> <DOT> <COM>)
+//
+
+#ifndef TARGET_STM32F103C6_SRC_UART_H_
+#define TARGET_STM32F103C6_SRC_UART_H_
+
+void uartConfigure();
+
+#endif  // TARGET_STM32F103C6_SRC_UART_H_

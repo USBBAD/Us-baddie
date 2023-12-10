d746a04d34c76ad51a97bab8aff953c8102c09b3
DmitryMurashov <dmtr.murashov@gmail.com>
2023-12-10 15:49:08 +0300

    [utility:chelp] Impl | Various C helpers

---
+++ src/common/utility/chelp.h
@@ -0,0 +1,15 @@
+//
+// chelp.h
+//
+// Created on: Dec 10, 2023
+//     Author: Dmitry Murashov (dmtr <DOT> murashov <AT> <GMAIL> <DOT> <COM>)
+//
+
+/// \brief Various helpers
+
+#ifndef USBAD_CHELP_H_
+#define USBAD_CHELP_H_
+
+#define USBAD_MIN(a, b) ((a) > (b) ? (b) : (a))
+
+#endif // USBAD_CHELP_H_

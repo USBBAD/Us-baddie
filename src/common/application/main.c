1c21ffa34847260d24f96828285763d567eed58d
DmitryMurashov <dmtr.murashov@gmail.com>
2024-01-06 12:42:27 +0300

    [application:main] Bug | Delay logic

--- src/common/application/main.c
+++ src/common/application/main.c
@@ -14,8 +14,9 @@ int main(void)
 	uartConfigure(1, 115200);
 
 	while (1) {
-		for (int delay = 5000; delay--;) {
-			uartTryPuts(1, "Hello, world!");
+		for (int delay = 5000000; delay--;) {
 		}
+
+		uartTryPuts(1, "Hello, world!");
 	}
 }

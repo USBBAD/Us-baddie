857c8cd33be7490fd8b2c3501ab8277fe8ac9668
DmitryMurashov <dmtr.murashov@gmail.com>
2024-01-08 13:47:37 +0300

    [stm32f103c6:stm32f103c6_dma] Impl | Carcass

---
+++ src/target/stm32f103c6/src/stm32f103c6_dma.c
@@ -0,0 +1,10 @@
+//
+// stm32f103c6_dma.c
+//
+// Created on: Jan 08, 2024
+//     Author: Dmitry Murashov (dmtr <DOT> murashov <AT> <GMAIL> <DOT> <COM>)
+//
+
+void stm32f103c6DmaUp()
+{
+}
---
+++ src/target/stm32f103c6/src/stm32f103c6_dma.h
@@ -0,0 +1,13 @@
+//
+// stm32f103c6_dma.h
+//
+// Created on: Jan 08, 2024
+//     Author: Dmitry Murashov (dmtr <DOT> murashov <AT> <GMAIL> <DOT> <COM>)
+//
+
+#ifndef SRC_TARGET_STM32F103C6_SRC_STM32F103C6_DMA_H_
+#define SRC_TARGET_STM32F103C6_SRC_STM32F103C6_DMA_H_
+
+void stm32f103c6DmaUp();
+
+#endif // SRC_TARGET_STM32F103C6_SRC_STM32F103C6_DMA_H_

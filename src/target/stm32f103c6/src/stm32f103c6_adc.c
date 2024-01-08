e80813fd7f7434beae9c975fe11ffc4dc76dfdf2
DmitryMurashov <dmtr.murashov@gmail.com>
2024-01-08 13:47:20 +0300

    [stm32f103c6:stm32f103c6_adc] Impl |

---
+++ src/target/stm32f103c6/src/stm32f103c6_adc.c
@@ -0,0 +1,39 @@
+//
+// stm32f103c6_adc.c
+//
+// Created on: Jan 08, 2024
+//     Author: Dmitry Murashov (dmtr <DOT> murashov <AT> <GMAIL> <DOT> <COM>)
+//
+
+#include <stm32f103x6.h>
+
+void stm32f103c6AdcUp()
+{
+	volatile ADC_TypeDef *adc = ADC1;
+
+	// Enable EOC (end of conversion) interrupt
+	adc->CR1 |= ADC_CR1_EOCIE;
+
+	// Enable SCAN mode (scan each of the enabled channels, one by one)
+	adc->CR1 |= ADC_CR1_SCAN;
+
+	// Enable DMA
+	adc->CR2 |= ADC_CR2_DMA;
+
+	// Enable continuous mode (re-enable conversion after it is finished)
+	adc->CR2 |= ADC_CR2_CONT;
+
+	// Enable conversion
+//	adc->CR2 |= ADC_CR2_ADON; // TODO: should it be enabled later?
+
+	// Configure conversion sequence
+
+	// 2 conversions
+	adc->SQR1 |= ADC_SQR1_L_0;
+
+	// Use ADC12_IN1 (PA1) as first conversion
+	adc->SQR3 |= 1;
+
+	// Use ADC12_IN2 (PA2) as second conversion
+	adc->SQR3 |= 2 << 5;
+}
---
+++ src/target/stm32f103c6/src/stm32f103c6_adc.h
@@ -0,0 +1,15 @@
+//
+// stm32f103c6_adc.h
+//
+// Created on: Jan 08, 2024
+//     Author: Dmitry Murashov (dmtr <DOT> murashov <AT> <GMAIL> <DOT> <COM>)
+//
+
+#ifndef SRC_TARGET_STM32F103C6_SRC_STM32F103C6_ADC_H_
+#define SRC_TARGET_STM32F103C6_SRC_STM32F103C6_ADC_H_
+
+/// \brief Initializes ADC
+/// \pre Clock must be initialized
+void stm32f103c6AdcUp();
+
+#endif // SRC_TARGET_STM32F103C6_SRC_STM32F103C6_ADC_H_

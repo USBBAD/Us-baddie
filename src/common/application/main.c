8a34dee817a76fa57c42553428abaa84ec73553f
DmitryMurashov <dmtr.murashov@gmail.com>
2024-02-04 17:45:16 +0300

    [application:main] Impl | ADC ISR, ADC startup

--- src/common/application/main.c
+++ src/common/application/main.c
@@ -5,15 +5,24 @@
 //  Author: Dmitry Murashov (dmtr <DOT> murashov <AT> GMAIL)
 //
 
+#include "hal/adc.h"
+#include "hal/dma.h"
 #include "hal/uart.h"
 #include "target/target.h"
+
+void onAdcIsr()
+{
+	volatile uint16_t *buffer = dmaGetBufferIsr(1, 1);
+}
 
 int main(void)
 {
 	targetInitialize();
 	uartConfigure(1, 115200);
+	dmaSetIsrHook(1, 1, onAdcIsr);
+	adcStart();
 
 	while (1) {
-		uartTryPuts(1, "Hello, world!");
+//		uartTryPuts(1, "Hello, world!");
 	}
 }

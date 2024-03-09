81c4f408c7f3dcdfa9571471f46b4a4c9ccf2b09
DmitryMurashov <dmtr.murashov@gmail.com>
2024-03-09 11:49:25 +0300

    [stm32f103c6:clock] Bug | Reconfigured clock to satisfy USB requirements regarding clock source
    - USB requres HSE to be used as a clock source;
    - Through experiments, it has been established that HSI will suffice;
    	- Regardless of the reason for that, sticking to datasheet is both viable and safe option

--- src/target/stm32f103c6/src/clock.c
+++ src/target/stm32f103c6/src/clock.c
@@ -9,8 +9,58 @@
 #include <stm32f103x6.h>
 #include <stdint.h>
 
+static void clockInitializeHse72Mhz();
+static void clockInitializeHsi48Mhz();
+
+// \details Necessary to get USB operating (stm32f103x6 datasheet, DocID15060
+// Rev 7)
+// RCC APB1 frequency = 72000000;
+// RCC APB2 frequency = 36000000;
+// RCC AHB frequency = 72000000;
+static void clockInitializeHse72Mhz()
+{
+	volatile RCC_TypeDef *rcc = RCC;
+	// Enable Internal 8 MHz HSI clock source
+	rcc->CR |= RCC_CR_HSEON;
+
+	// Wait until HSE is ready
+	while (!(rcc->CR & RCC_CR_HSERDY));
+
+	// APB 1, 36 MHz max, should not exceed, div. SYSCLK
+	rcc->CFGR |= RCC_CFGR_PPRE1_DIV2;
+
+	// Enable 1.5 prescaler to get 48MHz on USB bus
+	rcc->CFGR &= ~RCC_CFGR_USBPRE;
+
+	{
+		// High clock requires setting latency on flash memory access, RM0008 rev. 21
+		volatile FLASH_TypeDef *flash = FLASH;
+		uint32_t ws = 2;
+		// TODO: iffy, as it was imported from CDCACM code
+		volatile uint32_t reg32 = flash->ACR;
+		reg32 &= ~(FLASH_ACR_LATENCY_Msk << FLASH_ACR_LATENCY_Pos);
+		reg32 |= (ws << FLASH_ACR_LATENCY_Pos);
+		flash->ACR = reg32;
+	}
+
+	// Set PLLMUL x 9
+	rcc->CFGR |= RCC_CFGR_PLLMULL9;  // TODO iffy
+
+	// Select external as PLL clock source
+	rcc->CFGR |= RCC_CFGR_PLLSRC;
+
+	// Enable PLL
+	rcc->CR |= RCC_CR_PLLON;
+
+	// Wait for PLL to get ready
+	while (!(rcc->CR & RCC_CR_PLLRDY));
+
+	// Use PLL as the SYSCLK source (8 MHz external oscillator)
+	rcc->CFGR |= RCC_CFGR_SW_PLL;
+}
+
 /// \brief 48 MHz at SYSCLK, HSI as system clock, USB-compatible
-void clockInitialize()
+static void clockInitializeHsi48Mhz()
 {
 	volatile RCC_TypeDef *rcc = RCC;
 	// Enable Internal 8 MHz HSI clock source
@@ -38,6 +88,11 @@ void clockInitialize()
 	rcc->APB1ENR |= RCC_APB1ENR_USBEN;
 
 	uartUp();
+}
+
+void clockInitialize()
+{
+	clockInitializeHse72Mhz();
 }
 
 uint32_t clockGetSysclkFrequency()

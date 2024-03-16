3afc0616e6194fa7161b4cd86656a0ac69834efc
DmitryMurashov <dmtr.murashov@gmail.com>
2024-03-16 20:18:52 +0300

    [stm32f103c6:usb.h] Impl |  Added header

---
+++ src/target/stm32f103c6/src/usb.h
@@ -0,0 +1,21 @@
+//
+// usb.h
+//
+// Created on: March 15, 2024
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#ifndef SRC_TARGET_STM32F103C6_SRC_USB_H_
+#define SRC_TARGET_STM32F103C6_SRC_USB_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+void usbInitialize();
+
+#ifdef __cplusplus
+}
+#endif  // __cplusplus
+
+#endif  // SRC_TARGET_STM32F103C6_SRC_USB_H_

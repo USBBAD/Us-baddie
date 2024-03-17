f4c845c934aa759ba2c501aad5c3819e9fd2144c
DmitryMurashov <dmtr.murashov@gmail.com>
2024-03-17 13:04:57 +0300

    [application] Impl | Implemented printf for logging

--- src/common/application/main.c
+++ src/common/application/main.c
@@ -9,6 +9,7 @@
 #include "hal/dma.h"
 #include "hal/uart.h"
 #include "target/target.h"
+#include "utility/usvprintf.h"
 
 void onAdcDmaIsr()
 {
@@ -17,12 +18,25 @@ void onAdcDmaIsr()
 	volatile uint16_t ch1 = buffer[0];
 }
 
+size_t uartPuts(const char *aBuffer, size_t aBufferLen)
+{
+	if (uartTryPutsLen(1, aBuffer, aBufferLen)) {
+		return aBufferLen;
+	}
+
+	return 0U;
+}
+
 int main(void)
 {
+	int val = 10;
 	targetInitialize();
 	uartConfigure(1, 115200);
+	usvprintfSetPuts(uartPuts);
+//	cbprintf(uartPuts, "Once Again %s %d\n", "yo", 20);
+	usvprintf("Once Again %s %d\n\r", "yo", 10);
 
 	while (1) {
-		uartTryPuts(1, "Hello, world!");
+//		uartTryPuts(1, "Hello, world!\n");
 	}
 }

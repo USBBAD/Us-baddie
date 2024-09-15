73a6aac0e91ae3d0aa225c16f3fdb4ae95b8ae9e
DmitryMurashov <dmtr.murashov@gmail.com>
2024-09-15 13:00:20 +0300

    [main] Ref | Removed dead code. structured the file

--- src/application/default/main.c
+++ src/application/default/main.c
@@ -1,9 +1,17 @@
-//
-// main.c
-//
-// Created: 2023-10-11
-//  Author: Dmitry Murashov (dmtr <DOT> murashov <AT> GMAIL)
-//
+/**
+ * main.c
+ *
+ * Created: 2023-10-11
+ *  Author: Dmitry Murashov
+ */
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included files
+ ****************************************************************************/
 
 #include "driver/usb_microphone/stub.h"
 #include "driver/usb_microphone/usb_microphone.h"
@@ -15,10 +23,30 @@
 #include "utility/debug.h"
 #include "utility/usvprintf.h"
 
+/****************************************************************************
+ * Private Types
+ ****************************************************************************/
+
 static uint16_t audioBuffer[2] = {0};
 static int sToken = -1;
 
-void onAdcDmaIsr()
+/****************************************************************************
+ * Private Function Prototypes
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+static void onAdcDmaIsr()
 {
 	usDebugPushMessage(sToken, "got DMA ISR");
 	uint16_t *buffer = dmaGetBufferIsr(1, 1);
@@ -27,25 +55,16 @@ void onAdcDmaIsr()
 	adcStopIsr();
 }
 
+/**
+ * @todo move into target/
+ */
 size_t uartPuts(const char *aBuffer, size_t aBufferLen)
 {
 	if (uartTryPutsLen(1, aBuffer, aBufferLen)) {
 		uartBusyWaitForWritten(1);
-
 		return aBufferLen;
 	}
-
 	return 0U;
-}
-
-static void printAudio(const void *aArg)
-{
-	usvprintf("Audio test L %u R %u\r\n", audioBuffer[0], audioBuffer[1]);
-}
-
-static void printStarted(const void *aArg)
-{
-	usvprintf("Application started\r\n");
 }
 
 static void taskRunSystick()
@@ -79,7 +98,6 @@ int main(void)
 {
 	int val = 10;
 	memoryInitialize();
-	sToken = usDebugRegisterToken("application");
 	targetInitialize();
 	uartConfigure(1, 921600);
 
@@ -87,8 +105,7 @@ int main(void)
 	usvprintfSetPuts(uartPuts);
 	dmaSetIsrHook(1, 1, onAdcDmaIsr);
 	adcStart();
-	usDebugAddTask(sToken, printStarted, 0);
-	usDebugPushMessage(sToken, "System is up");
+	usDebugPushMessage(0, "System is up");
 	usbMicrophoneInitUsbDriver();
 	usbMicrophoneInitStub();
 	usDebugSetLed(0, 0);
@@ -96,3 +113,7 @@ int main(void)
 	taskRunAudio();
 	taskRunSystick();
 }
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/

b134de29335d82291203fd532865985fd28b9528
DmitryMurashov <dmtr.murashov@gmail.com>
2024-09-15 14:27:55 +0300

    [target] Ref | File structure

--- src/target/target.h
+++ src/target/target.h
@@ -8,11 +8,51 @@
 #ifndef USBAD_SRC_COMMON_TARGET_TARGET_H_
 #define USBAD_SRC_COMMON_TARGET_TARGET_H_
 
+/****************************************************************************
+* Included Files
+****************************************************************************/
+
 #include "hal/adc.h"
 
+/****************************************************************************
+* Pre-processor Definitions
+****************************************************************************/
+
+/****************************************************************************
+* Public Types
+****************************************************************************/
+
+#ifndef __ASSEMBLY__
+
+/****************************************************************************
+* Public Data
+****************************************************************************/
+
+#ifdef __cplusplus
+#define EXTERN extern "C"
+extern "C"
+{
+#else
+#define EXTERN extern
+#endif
+
+/****************************************************************************
+* Public Function Prototypes
+****************************************************************************/
 /// \brief Performs all the target-dependent initialization
 /// \returns Target handle, if successful. 0 otherwise
 void targetInitialize();
 void memoryInitialize();
+
+/****************************************************************************
+ * Inline Functions
+ ****************************************************************************/
+
+#undef EXTERN
+#ifdef __cplusplus
+}
+#endif  /* __cplusplus */
+
+#endif  /* __ASSEMBLY__ */
 
 #endif  // USBAD_SRC_COMMON_TARGET_TARGET_H_
